d6c9c396adff72f289d184fab598de9d1afb11c3
User <user@Users-MacBook-Pro-7.local>
2018-01-11 21:36:12 +0100

    External Class file - to be on the same level as lab1 and lab2

---
+++ ExternalClassTest.java
@@ -0,0 +1,10 @@
+package pl.patryk.jipp.lab;
+
+import pl.patryk.jipp.lab.lab2.Lab2_Z3;
+
+public class ExternalClassTest {
+    public static void main(String[] args) {
+        ExternalClassTest externalClassTest = new ExternalClassTest();
+        //externalClassTest.
+    }
+}
